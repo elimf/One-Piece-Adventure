59b0d6544ceaf4322e4ddcbc9900aa545eee7b97
Elim Florvil <elim.florvil@icloud.com>
2023-10-30 23:27:07 +0100

    update

--- src/utils/jwtManager.utils.ts
+++ src/utils/jwtManager.utils.ts
@@ -35,6 +35,7 @@ export class JwtTokenManager {
   public isTokenValid(token: string): boolean {
     if (!token) return false
     const decodedToken = JSON.parse(atob(token.split('.')[1]))
+    console.log(decodedToken.exp * 1000 > Date.now())
     return decodedToken.exp * 1000 > Date.now()
   }
 
@@ -43,24 +44,16 @@ export class JwtTokenManager {
     if (!token) {
       return null
     }
+    // Faites une requête au serveur pour rafraîchir le token
+    const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/refreshToken`, {
+      token: token
+    })
 
-    try {
-      // Faites une requête au serveur pour rafraîchir le token
-      const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/refreshToken`, {
-        token: token
-      })
-
-      if (response.status === 200 && response.data && response.data.newToken) {
-        const newToken = response.data.newToken
-        this.setToken(newToken)
-        return newToken
-      } else {
-        // Gérez le cas où la requête échoue ou le serveur ne renvoie pas un nouveau token
-        return null
-      }
-    } catch (error) {
-      // Gérez les erreurs de la requête, par exemple, si le serveur est inaccessible
-      console.error('Erreur lors du rafraîchissement du token :', error)
+    if (response.status === 200 && response.data && response.data.newToken) {
+      const newToken = response.data.newToken
+      this.setToken(newToken)
+      return newToken
+    } else {
       return null
     }
   }
