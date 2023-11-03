d552a0373c37cce8c901c3795426e6aef204409f
Elim Florvil <elim.florvil@ceo-vision.com>
2023-11-03 12:53:28 +0100

    doc / refactor the code and add comment

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts">
-import CardForm from '../components/form/CardForm.vue'
-</script>
 <template>
   <div id="web">
     <CardForm page="register" />
   </div>
 </template>
+<script setup lang="ts">
+import CardForm from '../components/form/CardForm.vue'
+</script>
 <style scoped>
 #web {
   width: 100%;
