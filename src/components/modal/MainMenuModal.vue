0cd59d3904b2a04ba1f2145a90b929fef52a9869
Elim Florvil <90334513+elimf@users.noreply.github.com>
2023-11-05 03:40:40 +0100

    feat / Add main menu modal and navbar component

---
+++ src/components/modal/MainMenuModal.vue
@@ -0,0 +1,26 @@
+<!-- Modal.vue -->
+<template>
+  <div class=" relative inset-0 flex items-center justify-center h-max">
+    <div class="modal w-11/12 h-5/6 bg-gray-400">
+      <div class="modal-content p-4 rounded-lg shadow-lg relative">
+        <div class="flex justify-between items-center mb-4">
+          <h1 class="text-xl font-bold">{{ title }}</h1>
+        </div>
+        <div v-if="title === 'Game'">Contenu pour Game</div>
+        <div v-if="title === 'Personnages'">Contenu pour Personnages</div>
+        <div v-if="title === 'Crew'">Contenu pour Crew</div>
+        <div v-if="title === 'Statistiques'">Contenu pour Stats</div>  
+        <div v-if="title === 'Logout'">Contenu pour Logout</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: String 
+  },
+  methods: {}
+}
+</script>
--- src/views/MainMenuView.vue
+++ src/views/MainMenuView.vue
@@ -1,101 +1,30 @@
 <template>
-  <nav class="bg-amber-400 flex justify-center">
-    <ul  class="navbar-menu">
-      <li v-for="item in sidebarItems" :key="item.id" @click="showModal(item)"
-      >
-      <a class="group relative rounded-xl p-2 text-blue-600 hover:bg-blue-500 cursor-pointer">{{ item.tooltip }} </a>
-    </li>
-      <!-- <li><a>Game</a></li>
-      <li><a>Personnages</a></li>
-      <li><a>Crew</a></li>
-      <li><a>Statistiques </a></li>
-      <li><a>Logout </a></li> -->
-    </ul>
-  </nav>
-  <div>
-    <h1>Content here</h1>
-    <Modal v-if="isModalOpen" @close="closeModal" />
-  </div>
+  <NavbarComponent @navbarItemClick="updateModalTitle" />
+
+  <Modal :title="modalTitle" />
 </template>
+
 <script lang="ts">
-import Modal from '@/components/game/MenuGame.vue'
-import { AuthUtils } from '@/utils/auth.utils'
+import Modal from '@/components/modal/MainMenuModal.vue'
+import NavbarComponent from '@/components/navbar/NavbarComponent.vue'
+
 export default {
   components: {
-    Modal
+    Modal,
+    NavbarComponent
   },
   data() {
     return {
-      authUtils: new AuthUtils(),
-      open: false,
-      isModalOpen: false,
-      sidebarItems: [
-        {
-          id: 1,
-          tooltip: 'Layouts',
-          color: 'blue'
-        },
-        {
-          id: 2,
-          tooltip: 'Color Scheme',
-          color: 'gray'
-        },
-        {
-          id: 3,
-          tooltip: 'Layouts',
-          color: 'blue'
-        },
-        {
-          id: 4,
-          tooltip: 'Kaido',
-          color: 'gray'
-        },
-        {
-          id: 5,
-          tooltip: 'Inventory',
-          color: 'gray'
-        }
-      ]
+      modalTitle: "Game",
     }
   },
   methods: {
-    showModal(item: any) {
-      console.log('showModal', item.tooltip)
-      this.isModalOpen = true
-    },
-    closeModal() {
-      this.isModalOpen = false
-    },
-    dropdownEvent() {
-      console.log(this.open)
-      this.open = !this.open
-    },
-    logout() {
-      this.authUtils.logout()
+     updateModalTitle(title: string) {
+      this.modalTitle = title;
     }
   }
 }
 </script>
-<style scoped>
-.navbar-menu {
-  list-style: none;
-  float: right;
-  margin: 0;
-  padding: 0;
-}
 
-.navbar-menu li {
-  display: inline;
-  margin-left: 20px;
-}
 
-.navbar-menu li a {
-  text-decoration: none;
-  color: #fff;
-  font-size: 18px;
-}
 
-.navbar-menu li a:hover {
-  text-decoration: underline;
-}
-</style>
