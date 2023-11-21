e02c0796b051944694508782edcf442212f62173
Elim Florvil <elim.florvil@ceo-vision.com>
2023-11-21 15:00:04 +0100

    Refactor Login component to include form validation and submission logic

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,19 +1,136 @@
-import React from "react";
-//import { useAuth } from "../router/AuthContext";
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+
+interface FormDataLogin {
+  email: string;
+  password: string;
+}
+
+interface FormErrorsLogin {
+  email?: string;
+  password?: string;
+}
 
 const Login: React.FC = () => {
- // const { login } = useAuth();
+  const [formData, setFormData] = useState<FormDataLogin>({
+    email: "",
+    password: "",
+  });
 
-  const handleLogin = () => {
-    // Ajoutez ici votre logique de connexion avec l'API, par exemple.
-    // Après une connexion réussie, appelez la fonction login(token) du contexte.
+  const [errors, setErrors] = useState<FormErrorsLogin>({});
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors: FormErrorsLogin = {};
+
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+      valid = false;
+    }
+
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+      valid = false;
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (validateForm()) {
+      // Your form submission logic here
+      console.log("Form is valid, submit the data:", formData);
+    } else {
+      console.log("Form is not valid");
+    }
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   return (
-    <div>
-      <h2>Login Page</h2>
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Login
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          error={!!errors.email}
+          helperText={errors.email}
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <TextField
+          error={!!errors.password}
+          helperText={errors.password}
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </Button>
+
+        <Grid container>
+          <Grid item>
+            <Link href="/register" variant="body2">
+              {"Don't have an account? Sign Up"}
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   );
 };
 
